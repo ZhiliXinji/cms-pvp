dc1a0c53d95497a3583d7c6605d866ef43554b21
peop1e <cqq060213@163.com>
2025-03-10 20:00:17 +0800

    Fix time_limit_mutiplier in unittests

--- cms-test.sh
+++ cms-test.sh
@@ -2,5 +2,6 @@
 set -x
 
 GIT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+GIT_BRANCH_NAME=${GIT_BRANCH_NAME//\//-}
 
 docker compose -p $GIT_BRANCH_NAME -f docker-compose.test.yml run --build --rm testcms
